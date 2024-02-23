d77058d3a8a6eca2ae27d266cdbd8d5662548ca4
thrynchuk <thrynchuk@lohika.com>
2024-02-23 11:54:39 +0100

    use v1 version of lib

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/my-org-for-test/golang-project
+
+go 1.22
+
+require github.com/my-org-for-test/golang-lib v1.0.2
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/my-org-for-test/golang-lib/dependency"
+)
+
+func main() {
+	fmt.Println("Project 1: DependencyExample")
+	dependency.PrintDependencyMessage()
+}
